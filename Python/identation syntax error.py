d6578c99ba5cc8b0086b3a3d30ad58718208090b
hema-sasi <sajja.hemalathasasi@gmail.com>
2024-10-14 16:38:23 +0530

    k

---
+++ Python/identation syntax error.py
@@ -0,0 +1,7 @@
+if 5 > 2:
+print("Five is greater than two!")#Indentation refers to the spaces at the beginning of a code line.
+
+"""
+if 5 > 2:
+    print("Five is greater than two!")#this is the correct syntax
+    """
