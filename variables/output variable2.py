3c716840039e027e23b8a7e8c39ad62be2724cc5
hema-sasi <sajja.hemalathasasi@gmail.com>
2024-10-16 20:27:27 +0530

    k

---
+++ variables/output variable2.py
@@ -0,0 +1,7 @@
+#You can also use the + operator to output multiple variables
+#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
